03009fe8262322002d267daf4d5549270054443f
Jama Mohamed <jama3137@gmail.com>
2020-01-16 17:20:06 +0300

    Set the results page UI

--- app/src/main/java/com/jama/receipttextrecognitionapp/ResultsFragment.kt
+++ app/src/main/java/com/jama/receipttextrecognitionapp/ResultsFragment.kt
@@ -3,10 +3,16 @@ package com.jama.receipttextrecognitionapp
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.jama.receipttextrecognitionapp.services.GiphyAPI
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class ResultsFragment : Fragment() {
 
@@ -20,5 +26,15 @@ class ResultsFragment : Fragment() {
         rootView = inflater.inflate(R.layout.fragment_results, container, false)
         return rootView
     }
+
+
+//    private fun loadGif() {
+//        CoroutineScope(Dispatchers.IO).launch {
+//            successResponse = GiphyAPI().getGif("success").data.imageUrl
+//            delay(2000)
+//            failureResponse = JsonPlaceHolderAPI().getGif("failure").data.imageUrl
+//            Log.e("jjj", "$successResponse $failureResponse")
+//        }
+//    }
 
 }
