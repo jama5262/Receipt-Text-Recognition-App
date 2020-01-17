506e7ea76dcca119c4b725d6541c71b1906eae4d
Jama Mohamed <jama3137@gmail.com>
2020-01-17 14:21:23 +0300

    Setup gif

--- app/src/main/java/com/jama/receipttextrecognitionapp/ResultsFragment.kt
+++ app/src/main/java/com/jama/receipttextrecognitionapp/ResultsFragment.kt
@@ -1,17 +1,15 @@
 package com.jama.receipttextrecognitionapp
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.jama.receipttextrecognitionapp.services.GiphyAPI
+import kotlinx.android.synthetic.main.fragment_results.view.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class ResultsFragment : Fragment() {
@@ -24,17 +22,48 @@ class ResultsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_results, container, false)
+
+        rootView.customButtonTryAgain.visibility = View.GONE
+        rootView.imageView.visibility = View.GONE
+        rootView.progressBar.visibility = View.GONE
+
+        rootView.customButtonYes.setOnClickListener {
+            disableButtons()
+            loadGif("success")
+        }
+
+        rootView.customButtonNo.setOnClickListener {
+            disableButtons()
+            loadGif("failure")
+        }
+
+        rootView.customButtonTryAgain.setOnClickListener {
+            activity?.onBackPressed()
+        }
+
         return rootView
     }
 
+    private fun disableButtons() {
+        rootView.customButtonYes.isEnabled = false
+        rootView.customButtonNo.isEnabled = false
+        rootView.customButtonTryAgain.visibility = View.VISIBLE
+        rootView.imageView.visibility = View.VISIBLE
+        rootView.progressBar.visibility = View.VISIBLE
+    }
 
-//    private fun loadGif() {
-//        CoroutineScope(Dispatchers.IO).launch {
-//            successResponse = GiphyAPI().getGif("success").data.imageUrl
-//            delay(2000)
-//            failureResponse = JsonPlaceHolderAPI().getGif("failure").data.imageUrl
-//            Log.e("jjj", "$successResponse $failureResponse")
-//        }
-//    }
+
+    private fun loadGif(tag: String) {
+        CoroutineScope(Dispatchers.Main).launch {
+            val response = GiphyAPI().getGif(tag).data.imageUrl
+            Glide
+                .with(rootView.context)
+                .load(response)
+                .into(rootView.imageView)
+            rootView.progressBar.visibility = View.GONE
+        }
+    }
+
+
 
 }
